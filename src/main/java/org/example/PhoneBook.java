70aa7856f381f71cdaab0d90a14f89abae07a652
Ilya Kamyshov <kamyshov@gmail.com>
2024-02-21 15:47:29 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import java.util.TreeMap;
+
 public class PhoneBook {
 
+    private static TreeMap<String, String> phoneBook = new TreeMap<>();
+
     public static int add(String name, String phoneNumber) {
-        return 0;
+        phoneBook.put(name, phoneNumber);
+        return phoneBook.size();
     }
 
 }
